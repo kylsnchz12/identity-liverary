f853b4b2a1809a57669c56a3f23c2bee15ce86ec
Madneis <arvicmicah.gingoyon.21@usjr.edu.ph>
2023-11-10 02:39:42 +0800

    categories, trainings, topics, and resources controllers added.

---
+++ Controllers/ResourcesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using liveraryIdentity.Data;
+using liveraryIdentity.Models;
+
+namespace liveraryIdentity.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ResourcesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Resources
+        public async Task<IActionResult> Index()
+        {
+              return _context.Resources != null ? 
+                          View(await _context.Resources.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Resources'  is null.");
+        }
+
+        // GET: Resources/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Resources == null)
+            {
+                return NotFound();
+            }
+
+            var resource = await _context.Resources
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            return View(resource);
+        }
+
+        // GET: Resources/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Resources/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,TopicID,FilePath")] Resource resource)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(resource);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(resource);
+        }
+
+        // GET: Resources/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Resources == null)
+            {
+                return NotFound();
+            }
+
+            var resource = await _context.Resources.FindAsync(id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+            return View(resource);
+        }
+
+        // POST: Resources/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,TopicID,FilePath")] Resource resource)
+        {
+            if (id != resource.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(resource);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ResourceExists(resource.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(resource);
+        }
+
+        // GET: Resources/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Resources == null)
+            {
+                return NotFound();
+            }
+
+            var resource = await _context.Resources
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            return View(resource);
+        }
+
+        // POST: Resources/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Resources == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Resources'  is null.");
+            }
+            var resource = await _context.Resources.FindAsync(id);
+            if (resource != null)
+            {
+                _context.Resources.Remove(resource);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ResourceExists(int id)
+        {
+          return (_context.Resources?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/TrainingsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using liveraryIdentity.Data;
+using liveraryIdentity.Models;
+
+namespace liveraryIdentity.Controllers
+{
+    public class TrainingsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TrainingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Trainings
+        public async Task<IActionResult> Index()
+        {
+              return _context.Trainings != null ? 
+                          View(await _context.Trainings.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Trainings'  is null.");
+        }
+
+        // GET: Trainings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Trainings == null)
+            {
+                return NotFound();
+            }
+
+            var training = await _context.Trainings
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (training == null)
+            {
+                return NotFound();
+            }
+
+            return View(training);
+        }
+
+        // GET: Trainings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Trainings/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,CategoryID,Title,Author,Description,DateCreated,Thumbnail")] Training training)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(training);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(training);
+        }
+
+        // GET: Trainings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Trainings == null)
+            {
+                return NotFound();
+            }
+
+            var training = await _context.Trainings.FindAsync(id);
+            if (training == null)
+            {
+                return NotFound();
+            }
+            return View(training);
+        }
+
+        // POST: Trainings/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,CategoryID,Title,Author,Description,DateCreated,Thumbnail")] Training training)
+        {
+            if (id != training.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(training);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TrainingExists(training.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(training);
+        }
+
+        // GET: Trainings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Trainings == null)
+            {
+                return NotFound();
+            }
+
+            var training = await _context.Trainings
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (training == null)
+            {
+                return NotFound();
+            }
+
+            return View(training);
+        }
+
+        // POST: Trainings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Trainings == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Trainings'  is null.");
+            }
+            var training = await _context.Trainings.FindAsync(id);
+            if (training != null)
+            {
+                _context.Trainings.Remove(training);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TrainingExists(int id)
+        {
+          return (_context.Trainings?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
